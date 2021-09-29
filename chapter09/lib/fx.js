1f02f63762a629a5711256a1bab6ebd1a88a6386
charmingkyu <loadkrnis@gmail.com>
2021-09-30 01:07:36 +0900

    Fix: 수정 강의 9-3

--- chapter09/lib/fx.js
+++ chapter09/lib/fx.js
@@ -7,18 +7,19 @@ const isIterable = a => a && a[Symbol.iterator];
 
 const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
 
+const reduceF = (acc, a, f) =>
+  a instanceof Promise ? a.then(a => f(acc, a), e => e === nop ? acc : Promise.reject(e)) : f(acc, a)
+
+const head = iter => go1(take(1, iter), ([h]) => h);
+
 const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  } else {
-    iter = iter[Symbol.iterator]();
-  }
+  if (!iter) return reduce(f, head(iter = acc[Symbol.iterator]()), iter);
+
+  iter = iter[Symbol.iterator]();
   return go1(acc, function recur(acc) {
     let cur;
     while (!(cur = iter.next()).done) {
-      const a = cur.value;
-      acc = f(acc, a);
+      acc = reduceF(acc, cur.value, f);
       if (acc instanceof Promise) return acc.then(recur);
     }
     return acc;
